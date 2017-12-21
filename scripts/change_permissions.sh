a6f0cbd5b539510ecaeabfd5f3e56b30c006542a
Justin Emmanuel S. Mercado <justinemmanuels.mercado@Plover.local>
2017-12-21 17:31:05 +0800

    added source

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 chmod -R 755 /home/ec2-user/node-app
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+source /home/ec2-user/.bash_profile
 isExistApp=`pgrep nginx`
 if [[ -n  $isExistApp ]]; then
    service nginx stop
