d60a2573dd1d742179ae5988901da758929e50c7
Justin Emmanuel S. Mercado <justinemmanuels.mercado@Plover.local>
2017-12-21 18:25:43 +0800

    ang hirap

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 sudo service nginx restart
-nvm use 9.3.0
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.6/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 9.3.0
 npm run start
