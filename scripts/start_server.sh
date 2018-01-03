5a8589f628835fae8eba4c5e54a044c69c9ec5c6
Justin Emmanuel S. Mercado <justinemmanuels.mercado@Plover.local>
2018-01-03 11:06:52 +0800

    changed start and stop script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,4 +3,5 @@ source /home/ec2-user/.bash_profile
 source /home/ec2-user/.bashrc
 cd /home/ec2-user/node-app
 sudo service nginx restart
+npm run stop
 npm run start
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -3,7 +3,3 @@ isExistApp=`pgrep nginx`
 if [[ -n  $isExistApp ]]; then
    sudo service nginx stop
 fi
-isExistApp=`pgrep node`
-if [[ -n  $isExistApp ]]; then
-    pm2 kill
-fi
