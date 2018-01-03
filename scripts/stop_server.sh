730c289cbd0cc1ee14c52ea2cb852e38b6ee72f3
Justin Emmanuel S. Mercado <justinemmanuels.mercado@Plover.local>
2018-01-03 11:23:14 +0800

    wtf is wrong

--- scripts/stop_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-isExistApp=`pgrep nginx`
-if [[ -n  $isExistApp ]]; then
-   sudo service nginx stop
-fi
-isExistApp=`pgrep nginx`
-if [[ -n  $isExistApp ]]; then
-   sudo service nginx stop
-fi
